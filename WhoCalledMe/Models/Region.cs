80459e36f840afbe8187f31de7db991700625cb5
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2018-01-15 17:49:14 +0300

    [+] Models : added basic models

---
+++ WhoCalledMe/Models/Region.cs
@@ -0,0 +1,9 @@
+﻿namespace WhoCalledMe.Models
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TimeZoneSpan { get; set; }
+    }
+}
