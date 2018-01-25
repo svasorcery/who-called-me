a0cdc3780ae8a0ff5da0c567827f7afbef5cba79
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-01-25 23:41:50 +0300

    [!] Phone : fixed missing Id bug in model

--- WhoCalledMe/Models/Phone.cs
+++ WhoCalledMe/Models/Phone.cs
@@ -2,6 +2,7 @@
 {
     public class Phone
     {
+        public int Id { get; set; }
         public string CountryCode { get; set; }
         public string Prefix { get; set; }
         public string Number { get; set; }
