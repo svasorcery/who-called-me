53dd5c1c8cbd2673bd057f9bfd629a96a4e5ee4e
Vladimir Sinyavsky <sva.sorcery@gmail.com>
2018-01-25 23:40:09 +0300

    [+] DbContext : Created PhonebookDbContext

---
+++ WhoCalledMe/DbContexts/PhonebookDbContext.cs
@@ -0,0 +1,16 @@
+﻿using WhoCalledMe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WhoCalledMe.DbContexts
+{
+    public class PhonebookDbContext : DbContext
+    {
+        public PhonebookDbContext(DbContextOptions<PhonebookDbContext> options) 
+            : base(options)
+        { }
+
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Operator> Operators { get; set; }
+        public DbSet<Region> Regions { get; set; }
+    }
+}
--- WhoCalledMe/Startup.cs
+++ WhoCalledMe/Startup.cs
@@ -5,16 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WhoCalledMe
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<DbContexts.PhonebookDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
