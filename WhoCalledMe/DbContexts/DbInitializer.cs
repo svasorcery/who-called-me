c1796854eacdfc6dd559b1d174e872e34a37cdad
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2018-02-12 13:09:37 +0300

    [+] Added DbInitializer for insertion data into database, if not exists

---
+++ WhoCalledMe/DbContexts/DbInitializer.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace WhoCalledMe.DbContexts
+{
+    public static partial class DbInitializer
+    {
+        public static void Initialize(PhonebookDbContext context)
+        {
+            context.Database.EnsureCreated();
+            
+            if (!context.Regions.Any())
+            {
+                context.Regions.AddRange(_regions);
+                context.SaveChanges();
+            }
+
+            if (!context.Operators.Any())
+            {
+                context.Operators.AddRange(_operators);
+                context.SaveChanges();
+            }
+
+            if (!context.DefCodes.Any())
+            {
+                context.DefCodes.AddRange(_defCodes);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- WhoCalledMe/Models/Phone.cs
+++ WhoCalledMe/Models/Phone.cs
@@ -2,7 +2,6 @@
 {
     public class Phone
     {
-        public int Id { get; set; }
         public string CountryCode { get; set; }
         public string Prefix { get; set; }
         public string Number { get; set; }
