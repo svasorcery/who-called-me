b30c3f1e8bce9f38d8c1fe86087a8332e0364906
Vladimir Sinyavsky <siniavsky.vladimir@gmail.com>
2018-02-12 12:23:42 +0300

    [+] DbContext : Added PhonebookDbContext, configured connection string

---
+++ WhoCalledMe/DbContexts/PhonebookDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WhoCalledMe.Entities;
+
+namespace WhoCalledMe.DbContexts
+{
+    public class PhonebookDbContext : DbContext
+    {
+        public PhonebookDbContext(DbContextOptions<PhonebookDbContext> options)
+            : base(options)
+        { }
+
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<DefCode> DefCodes { get; set; }
+        public DbSet<Operator> Operators { get; set; }
+    }
+}
